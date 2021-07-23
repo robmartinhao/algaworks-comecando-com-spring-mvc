76fd6dacf07c3c37b16251b502a08791a6e50bf0
Robson <robmartinhao@gmail.com>
2021-07-23 19:55:52 -0300

    infra inicial

---
+++ src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/AlgaworksComecandoComSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.algaworkscomecandocomspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksComecandoComSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaworksComecandoComSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/algaworks/algaworkscomecandocomspringmvc/AlgaworksComecandoComSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.algaworkscomecandocomspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksComecandoComSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
