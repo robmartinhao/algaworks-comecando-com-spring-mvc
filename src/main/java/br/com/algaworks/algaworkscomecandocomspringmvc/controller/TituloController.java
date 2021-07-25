35ff0942c466c4ac6c3dace92d68bb732ae94aa5
Robson <robmartinhao@gmail.com>
2021-07-25 14:01:20 -0300

    2.2. Protótipo do cadastro de título

---
+++ src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/controller/TituloController.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.algaworkscomecandocomspringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TituloController {
+
+    @RequestMapping("/titulos/novo")
+    public String novo() {
+        return "CadastroTitulo";
+    }
+}
