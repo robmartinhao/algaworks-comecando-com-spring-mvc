35beeacf13b81eff8bfa78366bc760b1f835a818
Robson <robmartinhao@gmail.com>
2021-07-25 19:55:39 -0300

    2.3. Enviando os dados para o servidor

--- src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/controller/TituloController.java
+++ src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/controller/TituloController.java
@@ -1,13 +1,25 @@
 package br.com.algaworks.algaworkscomecandocomspringmvc.controller;
 
+import br.com.algaworks.algaworkscomecandocomspringmvc.model.Titulo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 
-    @RequestMapping("/titulos/novo")
+    @RequestMapping("/novo")
     public String novo() {
         return "CadastroTitulo";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String salvar(Titulo titulo) {
+
+        System.out.println(">>> " + titulo.getDescricao());
+
+        return "CadastroTitulo";
+
     }
 }
