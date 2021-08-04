e0bfd6d4a0a1cf175750b0ead091be41482851b1
Robson <robmartinhao@gmail.com>
2021-08-03 21:48:17 -0300

    2.10. Listando os títulos

--- src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/model/Titulo.java
+++ src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/model/Titulo.java
@@ -66,6 +66,10 @@ public class Titulo {
         this.status = status;
     }
 
+    public boolean isPendente() {
+        return StatusTitulo.PENDENTE.equals(this.status);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
