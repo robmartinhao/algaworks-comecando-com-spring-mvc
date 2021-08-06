9a801eca4149b73bc34e044ecb68ddcd03a76e55
Robson <robmartinhao@gmail.com>
2021-08-05 21:20:44 -0300

    2.15. Implementando a edição

--- src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/controller/TituloController.java
+++ src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,12 +21,14 @@ import java.util.List;
 @RequestMapping("/titulos")
 public class TituloController {
 
+    private static final String CADASTRO_VIEW = "CadastroTitulo";
+
     @Autowired
     private Titulos titulos;
 
     @RequestMapping("/novo")
     public ModelAndView novo() {
-        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
         mv.addObject(new Titulo());
         return mv;
     }
@@ -33,9 +36,9 @@ public class TituloController {
     @RequestMapping(method = RequestMethod.POST)
     public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 
-        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
         if (errors.hasErrors()) {
-            return "CadastroTitulo";
+            return CADASTRO_VIEW;
         }
         titulos.save(titulo);
         attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
@@ -49,6 +52,15 @@ public class TituloController {
         mv.addObject("titulos", todosTitulos);
         return mv;
     }
+
+    @RequestMapping("{codigo}")
+    public ModelAndView edicao(@PathVariable Long codigo) {
+        Titulo titulo = titulos.findById(codigo).orElse(null);
+        ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+        mv.addObject(titulo);
+        return mv;
+    }
+
 
     @ModelAttribute("todosStatus")
     public List<StatusTitulo> todosStatusTitulo() {
