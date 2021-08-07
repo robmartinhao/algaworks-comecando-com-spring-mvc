fcf96e08608fd6ad422dd6253f55760235a642c2
Robson <robmartinhao@gmail.com>
2021-08-07 12:55:26 -0300

    3.3. Máscara de entrada de valores

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -1,4 +1,4 @@
-$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget);
 
     var codigoTitulo = button.data('codigo');
@@ -7,7 +7,7 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
     var modal = $(this);
     var form = modal.find('form');
     var action = form.data('url-base');
-    if(!action.endsWith('/')) {
+    if (!action.endsWith('/')) {
         action += '/';
     }
     form.attr('action', action + codigoTitulo);
@@ -15,6 +15,8 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
     modal.find('.modal-body p').html('Tem certeza que deseja excluir o título <strong>' + descricaoTitulo + '</strong>?');
 });
 
-// $(function() {
-//     $('[rel="tooltip"]').tooltip();
-// });
+$(function () {
+    // $('[rel="tooltip"]').tooltip();
+    $('.js-currency').maskMoney({decimal: ',', thousands: '.', allowZero: true});
+});
+
