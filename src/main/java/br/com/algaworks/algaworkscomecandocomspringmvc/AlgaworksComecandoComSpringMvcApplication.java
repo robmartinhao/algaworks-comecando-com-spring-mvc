dde12f904083e63e591fcde316a609c93e1aa063
Robson <robmartinhao@gmail.com>
2021-08-13 17:34:33 -0300

    3.11. Context path no Tomcat externo

--- src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/AlgaworksComecandoComSpringMvcApplication.java
+++ src/main/java/br/com/algaworks/algaworkscomecandocomspringmvc/AlgaworksComecandoComSpringMvcApplication.java
@@ -3,7 +3,11 @@ package br.com.algaworks.algaworkscomecandocomspringmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 import java.util.Locale;
@@ -18,5 +22,14 @@ public class AlgaworksComecandoComSpringMvcApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+
+	@Configuration
+	public static class MvcConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
 	}
 }
